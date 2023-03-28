42142fdcc782abf4f6a9e871caa0f6fbc22ed86d
Valentyna Vyrodova <valya.vyrodova@gmail.com>
2023-03-28 20:35:44 +0300

    done checkout page and begun about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,22 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title='about'/>
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>Occaecat proident cillum ea ex fugiat pariatur velit aute et consectetur veniam esse. Aliqua occaecat in cillum ex velit voluptate eu enim sunt in eu tempor officia laboris. Qui laboris consectetur incididunt minim consequat nisi et duis irure sit consequat quis. Enim commodo officia in culpa esse irure eu ex ut labore ex culpa aliquip. Eu aute in fugiat ut nisi labore enim veniam eiusmod excepteur amet. Est in ut Lorem anim laboris. Nulla voluptate laborum enim voluptate occaecat.</p>
+        </article>
+
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.section`

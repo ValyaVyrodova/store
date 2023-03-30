1e8ac3a0b6586f11ce9b09bac2d488ec8fa7e90f
Valentyna Vyrodova <valya.vyrodova@gmail.com>
2023-03-30 17:01:41 +0300

    completed fetch products

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,6 +15,10 @@ import {
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  product: [],
+  featured_products: [],
 }
 
 const ProductsContext = React.createContext()
@@ -25,12 +29,28 @@ export const ProductsProvider = ({ children }) => {
   const openSidebar = () => {
     dispatch({ type: SIDEBAR_OPEN })
   }
+
   const closeSidebar = () => {
     dispatch({ type: SIDEBAR_CLOSE })
   }
 
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN })
+    try {
+      const response = await axios.get(url)
+      const products = response.data
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
+    } catch (error) {
+      dispatch({type: GET_PRODUCTS_ERROR})
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts(url)
+  }, [])
+
   return (
-    <ProductsContext.Provider value={{...state, openSidebar, closeSidebar}}>
+    <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
       {children}
     </ProductsContext.Provider>
   )

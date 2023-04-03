549527978f8e668ddd3e5510a892753268569e8e
Valentyna Vyrodova <valya.vyrodova@gmail.com>
2023-04-03 19:25:56 +0300

    implement fetch single product

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -19,6 +19,9 @@ const initialState = {
   products_error: false,
   product: [],
   featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product_product: {},
 }
 
 const ProductsContext = React.createContext()
@@ -41,7 +44,21 @@ export const ProductsProvider = ({ children }) => {
       const products = response.data
       dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
     } catch (error) {
-      dispatch({type: GET_PRODUCTS_ERROR})
+      dispatch({ type: GET_PRODUCTS_ERROR })
+    }
+  }
+
+  const fetchSingleProduct = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN })
+    try {
+      const response = await axios.get(url);
+      const singleProduct = response.data;
+      dispatch({
+        type: GET_SINGLE_PRODUCT_SUCCESS,
+        payload: singleProduct
+      })
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR })
     }
   }
 

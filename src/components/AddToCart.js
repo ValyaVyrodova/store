ec8d654902d26ce0170380548af3e0570e1a5922
Valentyna Vyrodova <valya.vyrodova@gmail.com>
2023-04-12 18:04:34 +0300

    added amount buttons

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -8,6 +8,29 @@ import AmountButtons from './AmountButtons'
 const AddToCart = ({ product }) => {
   const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount;
+    })
+  }
+
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount;
+    })
+
+  }
+
   return (
     <Wrapper>
       <div className='colors'>
@@ -28,7 +51,14 @@ const AddToCart = ({ product }) => {
         }</div>
       </div>
       <div className='btn-container'>
-
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
+        <Link to='/cart' className='btn'>
+          add to cart
+        </Link>
       </div>
 
     </Wrapper>

cf840a1095de250507b8f7ee048708ea09841ec9
Valentyna Vyrodova <valya.vyrodova@gmail.com>
2023-04-20 17:58:39 +0300

    implement setup and text input in filter func.

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -60,8 +60,16 @@ const filter_reducer = (state, action) => {
   }
 
   if (action.type === FILTER_PRODUCTS) {
-    console.log('filtering products')
-    return { ...state };
+    const { all_products } = state;
+    const {text, category, company, color, price, shipping} = state.filters;
+    let tempProducts = [...all_products]
+    // filtering
+    if(text) {
+      tempProducts = tempProducts.filter((product)=> {
+        return product.name.toLowerCase().startsWith(text)
+      })
+    }
+    return { ...state, filtered_products: tempProducts};
   }
 
   if (action.type === CLEAR_FILTERS) {

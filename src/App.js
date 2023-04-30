061b75ec586fbacec8dd7381d1add1faf902fd64
Valentyna Vyrodova <valya.vyrodova@gmail.com>
2023-04-30 15:19:27 +0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
 import {
@@ -21,27 +21,19 @@ function App() {
       <Router>
         <Navbar />
         <Sidebar />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route exact path='/about'>
-            <About />
-          </Route>
-          <Route exact path='/cart'>
-            <Cart />
-          </Route>
-          <Route exact path='/products'>
-            <ProductsPage />
-          </Route>
-          <Route exact path='/products/:id' children={<SingleProduct />} />
-          <PrivateRoute exact path='/checkout'>
-            <Checkout />
-          </PrivateRoute>
-          <Route path='*'>
-            <Error />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='products' element={<ProductsPage />} />
+          <Route path='products/:id' element={<SingleProduct />} />
+          <Route path='checkout' element={
+            <PrivateRoute>
+              <Checkout />
+            </PrivateRoute>
+          } />
+          <Route path='*' element={<Error />} />
+        </Routes>
         <Footer />
       </Router>
     </AuthWrapper>
